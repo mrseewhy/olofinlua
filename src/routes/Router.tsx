282951a62302afad53035b91542249703484a517
Olusegun Adeyemi <53488450+mrseewhy@users.noreply.github.com>
2026-02-19 15:22:27 +0100

    changed url again

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -5,7 +5,7 @@ import Layout from "../components/Layout";
 
 export const router = createBrowserRouter([
     {
-        path: '', element: <Layout />, children: [
+        path: '/', element: <Layout />, children: [
             { index: true, element: <Index /> }
         ]
     }
